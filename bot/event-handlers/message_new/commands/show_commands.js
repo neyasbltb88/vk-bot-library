327baabdfaae8fbbeccbe679a4fbbd0c2b7aef0f
Neyasbltb <neyasbltb_88@mail.ru>
2020-02-26 20:49:25 +0300

    Заполнен вывод списка с командами бота

--- bot/event-handlers/message_new/commands/show_commands.js
+++ bot/event-handlers/message_new/commands/show_commands.js
@@ -3,7 +3,23 @@ const DEFAULT = require('./default');
 module.exports = class extends DEFAULT {
     constructor(message) {
         super(message);
-        let answer = 'Запрос на показ команд бота';
+        let answer = `✅ Добавить запись в закладки с указанием категории:
+*bot_library добавь в категорию Тест
+{{ Заголовок записи }}
+Содержимое записи
+
+✅ Показать список категорий с записями:
+*bot_library категории
+
+✅ Показать содержимое категории:
+*bot_library категория git
+
+✅ Для перехода на другие страницы с записями категории:
+*bot_library категория git (2)
+
+✅ Показать содержимое записи с указанным id:
+*bot_library id:1
+`;
 
         this.reply(answer);
     }
